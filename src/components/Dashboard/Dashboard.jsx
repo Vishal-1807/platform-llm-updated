feab7610813209c150af41c0e1973d989b121c37
vinay-awone <vinay.maurya@awone.ai>
2025-10-28 00:12:21 +0530

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,7 +7,7 @@ import {
 } from "../../services/Portals/LLMPortals";
 import styles from "./Dashboard.module.css";
 export const Dashboard = () => {
-  const GrafanaApiUrl = "https://cibi.ai/cibi";
+  const GrafanaApiUrl = "https://awone-api.axiomio.com";
   const [active, setactive] = useState("Active");
 
   const [experimentInformation, setExperimentInformation] = useState([]);
@@ -66,11 +66,11 @@ export const Dashboard = () => {
     if (ProjectFilter === "llm") {
       const selectedValue = e.target.value.toLowerCase();
       setexpid(e.target.value);
-      const single = `https://awone-api.axiomio.com/cibi/llm-dev/grafana/d/a91763bb-d009-4be3-b378-2187a01e08bd/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk&width=450`;
+      const single = `https://awone-api.axiomio.com/grafana/d/a91763bb-d009-4be3-b378-2187a01e08bd/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk&width=450`;
       setFinalURL(single);
     } else {
       const selectedValue = ProjectID.toLowerCase();
-      const single = `https://awone-api.axiomio.com/cibi/llm-dev/grafana/d/a91763bb-d009-4be3-b378-2187a01e08bd/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk&width=450`;
+      const single = `https://awone-api.axiomio.com/grafana/d/a91763bb-d009-4be3-b378-2187a01e08bd/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk&width=450`;
       setFinalURL(single);
     }
   };
