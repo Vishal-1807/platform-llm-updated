ebf6f76ee985adb0df45b20df5b9da7b932fb055
Vishal Varma Kovoru <vishalvarmakovoru@Vishals-MacBook-Pro.local>
2025-10-30 19:23:24 +0530

    added kiosk=tv extension

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -66,11 +66,11 @@ export const Dashboard = () => {
     if (ProjectFilter === "llm") {
       const selectedValue = e.target.value.toLowerCase();
       setexpid(e.target.value);
-      const single = `https://awone-api.axiomio.com/grafana/d/a2159148-e5c7-4204-a1e6-fa614b5ebe6b/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk&width=450`;
+      const single = `https://awone-api.axiomio.com/grafana/d/a2159148-e5c7-4204-a1e6-fa614b5ebe6b/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk=tv&width=450`;
       setFinalURL(single);
     } else {
       const selectedValue = ProjectID.toLowerCase();
-      const single = `https://awone-api.axiomio.com/grafana/d/a2159148-e5c7-4204-a1e6-fa614b5ebe6b/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk&width=450`;
+      const single = `https://awone-api.axiomio.com/grafana/d/a2159148-e5c7-4204-a1e6-fa614b5ebe6b/kubecost-llm?orgId=1&refresh=10s&var-namespace=${selectedValue}-train&var-namespace=${selectedValue}-infer&var-namespace=${selectedValue}-preprocess&from=now-20d&to=now&theme=light&kiosk=tv&width=450`;
       setFinalURL(single);
     }
   };
@@ -101,7 +101,7 @@ export const Dashboard = () => {
       const namespaces = filteredExperiments
         .map((exp) => generateNamespaces(exp))
         .join("");
-      const finalUrl = `${baseUrl}${namespaces}&from=now-20d&to=now&theme=light&kiosk&width=450`;
+      const finalUrl = `${baseUrl}${namespaces}&from=now-20d&to=now&theme=light&kiosk=tv&width=450`;
       setFinalURL(finalUrl);
     } catch (error) {
       console.error("Error fetching experiments:", error);
@@ -136,7 +136,7 @@ export const Dashboard = () => {
       const namespaces = filteredExperiments
         .map((exp) => generateNamespaces(exp))
         .join("");
-      const finalUrl = `https://awone-api.axiomio.com/grafana/goto/y9dWnjgDg?orgId=1&from=now-20d&to=now&theme=light&kiosk&width=450`;
+      const finalUrl = `https://awone-api.axiomio.com/grafana/goto/y9dWnjgDg?orgId=1&from=now-20d&to=now&theme=light&kiosk=tv&width=450`;
       setFinalURL(finalUrl);
     } catch (error) {
       console.error("Error fetching experiments:", error);
