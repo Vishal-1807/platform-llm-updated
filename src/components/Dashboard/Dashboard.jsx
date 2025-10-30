c4d498028e59002f8f499c30f64ad16cd8525517
vinay-awone <vinay.maurya@awone.ai>
2025-10-30 16:23:55 +0530

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -143,7 +143,7 @@ export const Dashboard = () => {
     }
   };
 
-  const baseUrl = `https://awone-api.axiomio.com/grafana/goto/gv4mMCgvg?orgId=1&refresh=10s`;
+  const baseUrl = `https://awone-api.axiomio.com/grafana/public-dashboards/a571b2ce8ed84a8d9a305d6e0f34a26a&refresh=10s`;
   const generateNamespaces = (exp) => {
     const id =
       exp.proj_type === "tabular"
