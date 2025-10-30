87c158afa214efb5095fd76052954c2960ea57cb
Vishal Varma Kovoru <vishalvarmakovoru@Vishals-MacBook-Pro.local>
2025-10-30 17:42:53 +0530

    updated base url

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -143,7 +143,7 @@ export const Dashboard = () => {
     }
   };
 
-  const baseUrl = `${GrafanaApiUrl}/grafana/d/a2159148-e5c7-4204-a1e6-fa614b5ebe6b/kubecost-llm?orgId=1&refresh=10s`;
+  const baseUrl = `${GrafanaApiUrl}/grafana/d/2LaqbJ0Zk/kubecost?orgId=1&refresh=10s`;
   const generateNamespaces = (exp) => {
     const id =
       exp.proj_type === "tabular"
